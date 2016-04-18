75bc114b198783b2b9505d3c55736d2ad7c8d715
cs0x7f <cs0x7f@gmail.com>
2016-04-18 17:43:33 +0800

    SAE-kvdb to redis

--- dist/userdata.php
+++ dist/userdata.php
@@ -1,5 +1,7 @@
 <?php
-	if (!isset($_POST['id']) || empty($_POST['id'])) {
+	define('CSTIMER_PREFIX', 'prefix');
+
+	if (!isset($_POST['id']) || empty($_POST['id']) || strlen($_POST['id']) >= 250) {
 		echo '{"retcode":400}';
 		exit(0);
 	}
@@ -8,14 +10,24 @@
 		exit(0);
 	}
 
-	$kv = new SaeKV();
-	$ret = $kv->init();
 	header("Access-Control-Allow-Origin: *");
+
+	$redis = new Redis();
+	$redis->connect('127.0.0.1', 1234);
+
 	if (isset($_POST['data'])) {//SET
-		$kv->set('timeruserdata_'.$_POST['id'], $_POST['data']);
-		echo '{"retcode":0}';
+		if (!preg_match("/^[a-zA-Z0-9+\-]+$/", $_POST['data'])) {
+			echo '{"retcode":400}';
+			exit(0);
+		}
+		$result = $redis->set(CSTIMER_PREFIX . $_POST['id'], $_POST['data']);
+		if ($result === true) {
+			echo '{"retcode":0}';
+		} else {
+			echo '{"retcode":500}';
+		}
 	} else {//GET
-		$ret = $kv->get('timeruserdata_'.$_POST['id']);
+		$ret = $redis->get(CSTIMER_PREFIX . $_POST['id']);
 		if ($ret === false) {
 			echo '{"retcode":404}';
 			exit(0);
