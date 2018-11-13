d86bfe1505e2e90575a692a9d0161660948b1832
chenshuang <cs0x7f@gmail.com>
2018-11-13 15:44:44 +0800

    update stackmat

--- src/js/stackmat.js
+++ src/js/stackmat.js
@@ -9,10 +9,21 @@ var stackmat = (function() {
 	var bitAnalyzer;
 
 	function init(timer) {
-		var getUserMedia = (window.navigator["getUserMedia"] ||
-			window.navigator["webkitGetUserMedia"] ||
-			window.navigator["mozGetUserMedia"] ||
-			window.navigator["msGetUserMedia"]);
+		if (navigator.mediaDevices === undefined) {
+			navigator.mediaDevices = {};
+		}
+
+		if (navigator.mediaDevices.getUserMedia === undefined) {
+			navigator.mediaDevices.getUserMedia = function(constraints) {
+				var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+				if (!getUserMedia) {
+					return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+				}
+				return new Promise(function(resolve, reject) {
+					getUserMedia.call(navigator, constraints, resolve, reject);
+				});
+			}
+		}
 
 		var AudioContext = (window["AudioContext"] || window["webkitAudioContext"]);
 
@@ -30,12 +41,12 @@ var stackmat = (function() {
 		byteBuffer = [];
 
 		if (audio_stream == undefined) {
-			getUserMedia.call(window.navigator, {
+			navigator.mediaDevices.getUserMedia({
 				"audio": {
 					"echoCancellation": false,
 					"noiseSuppression": false
 				}
-			}, success, $.noop);
+			}).then(success, $.noop);
 		}
 	}
 
