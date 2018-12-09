a816331c52ca32c6773de5c8d1c03660986aa4a9
chenshuang <cs0x7f@gmail.com>
2018-12-09 21:38:05 +0800

    try importing from twisty timer

--- src/js/mathlib.js
+++ src/js/mathlib.js
@@ -582,6 +582,30 @@ if ('serviceWorker' in navigator) {
 
 var TimerDataConverter = (function() {
 
+	function readCSV(data, spliter) {
+		data = data.split(/\r?\n/g);
+		for (var i = 0; i < data.length; i++) {
+			var rawLine = data[i].split(spliter);
+			var line = [];
+			var curValue = [];
+			var cntQuote = 0;
+			for (var j = 0; j < rawLine.length; j++) {
+				curValue.push(rawLine[j]);
+				cntQuote += (rawLine[j].match(/"/g) || []).length;
+				if (cntQuote % 2 == 0) {
+					var append = curValue.join(',');
+					if (append[0] == '"') {
+						append = append.replace(/""/g, '"').slice(1, -1);
+					}
+					line.push(append);
+					curValue = [];
+				}
+			}
+			data[i] = line;
+		}
+		return data;
+	}
+
 	// {'name': [regex, converter]}
 	var Timers = {}
 
@@ -640,7 +664,45 @@ var TimerDataConverter = (function() {
 	}];
 
 	Timers['TwistyTimer'] = [/^Puzzle,Category,Time\(millis\),Date\(millis\),Scramble,Penalty,Comment/i, function(data) {
-
+		data = readCSV(data, ';');
+		var ScrambleMap = {
+			'333': '333',
+			'222': '222so',
+			'444': '444wca',
+			'555': '555wca',
+			'pyra': 'pyrso',
+			'skewb': 'skbso',
+			'mega': 'mgmp',
+			'sq1': 'sqrs',
+			'clock': 'clkwca',
+			'666': '666wca',
+			'777': '777wca'
+		};
+		var name2idx = {};
+		var ret = [];
+		for (var i = 1; i < data.length; i++) {
+			var line = data[i];
+			if (line.length != 7) {
+				continue;
+			}
+			var name = line[0] + '-' + line[1];
+			var time = [{
+				'0': 0,
+				'1': 2000,
+				'2': -1
+			}[line[5]], Math.round(line[2])];
+			if (!(name in name2idx)) {
+				name2idx[name] = ret.length;
+				ret.push({
+					'name': name,
+					'phases': 1,
+					'scr': ScrambleMap[line[0]],
+					'times': []
+				});
+			}
+			ret[name2idx[name]]['times'].push([time, line[4], line[6], Math.round(line[3] / 1000)]);
+		}
+		return ret;
 	}];
 
 	Timers['BlockKeeper'] = [/^{"puzzles":\[{"name":/i, function(data) {
@@ -671,15 +733,11 @@ var TimerDataConverter = (function() {
 				var sessionName = session['name'];
 				var timeList = [];
 				$.each(session['records'], function(idx, val) {
-					var time = [0, Math.round(val['time'] * 1000)];
-					switch (val['result']) {
-						case '+2':
-							time[0] = 2000;
-							break;
-						case 'DNF':
-							time[0] = -1;
-							break;
-					}
+					var time = [{
+						'OK': 0,
+						'+2': 2000,
+						'DNF': -1
+					}[val['result']], Math.round(val['time'] * 1000)];
 					Array.prototype.push.apply(time, $.map(val['split'].reverse(), function(value) {
 						return Math.round(value * 1000);
 					}));
