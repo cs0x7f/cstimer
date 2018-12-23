f817d7ffa0d7c17a25f8e9579146a92ee87f0245
chenshuang <cs0x7f@gmail.com>
2018-12-23 13:01:27 +0800

    clean up cross

--- src/js/tools/cross.js
+++ src/js/tools/cross.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var cross = execBoth(function(createMove, edgeMove, createPrun, setNPerm, getNPerm, Cnk, getPruning) {
+var cross = (function(createMove, edgeMove, createPrun, setNPerm, getNPerm, Cnk, getPruning) {
 	var permPrun, flipPrun, ecPrun, fullPrun;
 	var cmv = [];
 	var pmul = [];
@@ -212,10 +212,9 @@ var cross = execBoth(function(createMove, edgeMove, createPrun, setNPerm, getNPe
 
 	var curScramble;
 
-	function solve_cross(scramble, fdiv) {
+	function solve_cross(moves) {
 		init();
-		var moves = curScramble = kernel.parseScramble(scramble, "FRUBLD"); // = scramble.split(/\s+/);
-		fdiv.empty();
+		var ret = [];
 		for (var face = 0; face < 6; face++) {
 			var flip = 0;
 			var perm = 0;
@@ -229,27 +228,18 @@ var cross = execBoth(function(createMove, edgeMove, createPrun, setNPerm, getNPe
 			}
 			var sol = [];
 			var len = 0;
-			while (true) {
+			for (var len = 0; len < 100; len++) {
 				if (idacross(perm, flip, len, -1, sol)) {
 					break;
 				}
-				len++;
 			}
 			sol.reverse();
-			var span = $('<span />');
-			var clk = $('<span />').html('ec').addClass('click').click(ecClick);
-			span.append(faceStr[face] + "(", clk, "): " + rotIdx[face] + " " + sol.join("&nbsp;") + '<br>')
-			fdiv.append(span);
+			ret.push(sol);
 		}
+		return ret;
 	}
 
-	function ecClick() {
-		var span = $(this).parent();
-		var face = "DULRFB".indexOf(span.html()[0]);
-		solve_xcross(curScramble, face, span);
-	}
-
-	function solve_xcross(moves, face, span) {
+	function solve_xcross(moves, face) {
 		xinit();
 		var flip = 0;
 		var perm = 0;
@@ -280,7 +270,7 @@ var cross = execBoth(function(createMove, edgeMove, createPrun, setNPerm, getNPe
 			len++;
 		}
 		sol.reverse();
-		span.html(faceStr[face] + "(ec): " + rotIdx[face] + " " + sol.join("&nbsp;") + '<br>')
+		return sol;
 	}
 
 	function fullInit() {
@@ -326,170 +316,48 @@ var cross = execBoth(function(createMove, edgeMove, createPrun, setNPerm, getNPe
 		return [arrp, arrf];
 	}
 
-	function execFunc(fdiv) {
-		if (!fdiv) {
-			return;
-		}
-		var scramble = tools.getCurScramble();
-		if ("|333|333o|333oh|333fm|333ft|edges|easyc|".indexOf('|' + scramble[0] + '|') != -1 ||
-			scramble[0] == "input" && "|333|222o|".indexOf('|' + tools.scrambleType(scramble[1]) + '|') != -1) {
-			solve_cross(scramble[1], fdiv);
-		} else {
-			fdiv.html(IMAGE_UNAVAILABLE);
-		}
-	}
 
-	$(function() {
-		tools.regTool('cross', TOOLS_CROSS, execFunc);
+	execMain(function() {
+
+		function execCross(scramble, fdiv) {
+			fdiv.empty();
+			curScramble = kernel.parseScramble(scramble, "FRUBLD");
+			var solutions = solve_cross(curScramble);
+			for (var face = 0; face < 6; face++) {
+				var span = $('<span />');
+				var clk = $('<span />').html('ec').addClass('click').click(ecClick);
+				span.append(faceStr[face] + "(", clk, "): " + rotIdx[face] + " " + solutions[face].join("&nbsp;") + '<br>')
+				fdiv.append(span);
+			}
+		}
+
+		function ecClick() {
+			var span = $(this).parent();
+			var face = "DULRFB".indexOf(span.html()[0]);
+			var sol = solve_xcross(curScramble, face);
+			span.html(faceStr[face] + "(ec): " + rotIdx[face] + " " + sol.join("&nbsp;") + '<br>');
+		}
+
+		function execFunc(fdiv) {
+			if (!fdiv) {
+				return;
+			}
+			var scramble = tools.getCurScramble();
+			if ("|333|333o|333oh|333fm|333ft|edges|easyc|".indexOf('|' + scramble[0] + '|') != -1 ||
+				scramble[0] == "input" && "|333|222o|".indexOf('|' + tools.scrambleType(scramble[1]) + '|') != -1) {
+				execCross(scramble[1], fdiv);
+			} else {
+				fdiv.html(IMAGE_UNAVAILABLE);
+			}
+		}
+
+		$(function() {
+			tools.regTool('cross', TOOLS_CROSS, execFunc);
+		});
 	});
-
 
 	return {
 		solve: solve_cross,
 		getEasyCross: getEasyCross
 	}
-}, function(createMove, edgeMove, createPrun, setNPerm, getNPerm, Cnk, getPruning) {
-	var permPrun, flipPrun, ecPrun, fullPrun;
-	var cmv = [];
-	var pmul = [];
-	var fmul = [];
-
-	var e1mv = [];
-	var c1mv = [];
-
-	function pmv(a, c) {
-		var b = cmv[c][~~(a / 24)];
-		return 24 * ~~(b / 384) + pmul[a % 24][(b >> 4) % 24]
-	}
-
-	function fmv(b, c) {
-		var a = cmv[c][b >> 4];
-		return ~~(a / 384) << 4 | fmul[b & 15][(a >> 4) % 24] ^ a & 15
-	}
-
-	function i2f(a, c) {
-		for (var b = 3; 0 <= b; b--) c[b] = a & 1, a >>= 1
-	}
-
-	function f2i(c) {
-		for (var a = 0, b = 0; 4 > b; b++) a <<= 1, a |= c[b];
-		return a
-	}
-
-	function fullmv(idx, move) {
-		var slice = cmv[move][~~(idx / 384)];
-		var flip = fmul[idx & 15][(slice >> 4) % 24] ^ slice & 15;
-		var perm = pmul[(idx >> 4) % 24][(slice >> 4) % 24];
-		return (~~(slice / 384)) * 384 + 16 * perm + flip;
-	}
-
-	function init() {
-		init = $.noop;
-		for (var i = 0; i < 24; i++) {
-			pmul[i] = [];
-		}
-		for (var i = 0; i < 16; i++) {
-			fmul[i] = [];
-		}
-		var pm1 = [];
-		var pm2 = [];
-		var pm3 = [];
-		for (var i = 0; i < 24; i++) {
-			for (var j = 0; j < 24; j++) {
-				setNPerm(pm1, i, 4);
-				setNPerm(pm2, j, 4);
-				for (var k = 0; k < 4; k++) {
-					pm3[k] = pm1[pm2[k]];
-				}
-				pmul[i][j] = getNPerm(pm3, 4);
-				if (i < 16) {
-					i2f(i, pm1);
-					for (var k = 0; k < 4; k++) {
-						pm3[k] = pm1[pm2[k]];
-					}
-					fmul[i][j] = f2i(pm3);
-				}
-			}
-		}
-		createMove(cmv, 495, getmv);
-
-		permPrun = [];
-		flipPrun = [];
-		createPrun(permPrun, 0, 11880, 5, pmv);
-		createPrun(flipPrun, 0, 7920, 6, fmv);
-
-		//combMove[comb][m] = comb*, flip*, perm*
-		//newcomb = comb*, newperm = perm x perm*, newflip = flip x perm* ^ flip*
-		function getmv(comb, m) {
-			var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-			var r = 4;
-			for (var i = 0; i < 12; i++) {
-				if (comb >= Cnk[11 - i][r]) {
-					comb -= Cnk[11 - i][r--];
-					arr[i] = r << 1;
-				} else {
-					arr[i] = -1;
-				}
-			}
-			edgeMove(arr, m);
-			comb = 0, r = 4;
-			var t = 0;
-			var pm = [];
-			for (var i = 0; i < 12; i++) {
-				if (arr[i] >= 0) {
-					comb += Cnk[11 - i][r--];
-					pm[r] = arr[i] >> 1;
-					t |= (arr[i] & 1) << (3 - r);
-				}
-			}
-			return (comb * 24 + getNPerm(pm, 4)) << 4 | t;
-		}
-	}
-
-	function fullInit() {
-		fullInit = $.noop;
-		init();
-		fullPrun = [];
-		createPrun(fullPrun, 0, 190080, 7, fullmv, 6, 3, 6);
-	}
-
-	function getEasyCross(length) {
-		fullInit();
-		if (length > 8) {
-			length = 8;
-		}
-		var cases = mathlib.rn([1, 16, 174, 1568, 11377, 57758, 155012, 189978, 190080][length]) + 1;
-		var i;
-		for (i = 0; i < 190080; i++) {
-			if (getPruning(fullPrun, i) <= length && --cases == 0) {
-				break;
-			}
-		}
-		var comb = ~~(i / 384);
-		var perm = (i >> 4) % 24;
-		var flip = i & 15;
-
-		var arrp = [];
-		var arrf = [];
-		var pm = [];
-		var fl = [];
-		i2f(flip, fl);
-		setNPerm(pm, perm, 4);
-		var r = 4;
-		var map = [7, 6, 5, 4, 10, 9, 8, 11, 3, 2, 1, 0];
-		for (i = 0; i < 12; i++) {
-			if (comb >= Cnk[11 - i][r]) {
-				comb -= Cnk[11 - i][r--];
-				arrp[map[i]] = pm[r];
-				arrf[map[i]] = fl[r];
-			} else {
-				arrp[map[i]] = arrf[map[i]] = -1;
-			}
-		}
-		return [arrp, arrf];
-	}
-
-	return {
-		getEasyCross: getEasyCross
-	}
-}, [mathlib.createMove, mathlib.edgeMove, mathlib.createPrun, mathlib.setNPerm, mathlib.getNPerm, mathlib.Cnk, mathlib.getPruning]);
+})(mathlib.createMove, mathlib.edgeMove, mathlib.createPrun, mathlib.setNPerm, mathlib.getNPerm, mathlib.Cnk, mathlib.getPruning);
