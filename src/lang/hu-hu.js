b540ce86c8a83a725534fbd152dfc5c38d5bd253
chenshuang <cs0x7f@gmail.com>
2019-02-01 16:29:18 +0800

    update i18n

--- src/lang/hu-hu.js
+++ src/lang/hu-hu.js
@@ -1,42 +1,42 @@
 var OK_LANG = 'OK';
 var CANCEL_LANG = 'Mégse';
-var RESET_LANG = 'Reset';
+var RESET_LANG = 'Visszaállít';
 var ABOUT_LANG = 'RÓLUNK';
 var ZOOM_LANG = 'Nagyítás';
 var BUTTON_TIME_LIST = 'ERED-<br>MÉNYEK';
 var BUTTON_OPTIONS = 'BEÁL-<br>LÍTÁSOK';
-var BUTTON_EXPORT = 'EXPORT';
-var BUTTON_DONATE = 'DONATE';
+var BUTTON_EXPORT = 'EXPORTÁLÁS';
+var BUTTON_DONATE = 'ADOMÁNYOZÁS';
 var PROPERTY_USEINS = 'WCA memorizációs ideje';
-var PROPERTY_USEINS_STR = 'Always|Except BLD|Never';
-var PROPERTY_VOICEINS = 'voice alert of WCA inspection';
-var PROPERTY_VOICEINS_STR = 'none|male voice|female voice';
+var PROPERTY_USEINS_STR = 'Mindig|Vakot kivéve|Soha';
+var PROPERTY_VOICEINS = 'WCA megnézési figyelmeztetés hangja';
+var PROPERTY_VOICEINS_STR = 'nincs|férfi hang|női hang';
 var PROPERTY_PHASES = 'több fázis';
 var PROPERTY_TIMERSIZE = 'időmérő mérete';
 var PROPERTY_USEMILLI = 'milliszekundum használata';
-var PROPERTY_SMALLADP = 'use small font after decimal point';
+var PROPERTY_SMALLADP = 'kisebb számok használata a tizedespont után';
 var PROPERTY_SCRSIZE = 'keverés mérete';
 var PROPERTY_SCRMONO = 'keverés elülönítve';
 var PROPERTY_SCRLIM = 'a keverési terület magassága';
-var PROPERTY_SCRALIGN = 'Alignment of scramble area';
-var PROPERTY_SCRALIGN_STR = 'center|left|right';
-var PROPERTY_SCRFAST = 'Using fast scramble for 4x4x4 (non-official)';
+var PROPERTY_SCRALIGN = 'Keverési terület elhelyezése';
+var PROPERTY_SCRALIGN_STR = 'közép|bal|jobb';
+var PROPERTY_SCRFAST = 'Gyors keverés használata 4x4x4-hez (nem hivatalos)';
 var PROPERTY_SCRKEYM = 'Label key move(s) in scramble';
-var PROPERTY_WNDSCR = 'Scramble panel display style';
-var PROPERTY_WNDSTAT = 'Statistics panel display style';
-var PROPERTY_WNDTOOL = 'Tools panel display style';
-var PROPERTY_WND_STR = 'Normal|Flat';
-var EXPORT_DATAEXPORT = 'Data Import/Export';
-var EXPORT_TOFILE = 'Export to file';
-var EXPORT_FROMFILE = 'Import from file';
-var EXPORT_TOSERV = 'Export to server';
-var EXPORT_FROMSERV = 'Import from server';
-var EXPORT_FROMOTHER = 'Import session(s) from other timers';
-var EXPORT_USERID = 'Please input your account (only alphabet or number)';
-var EXPORT_INVID = 'Only alphabet or number is allowed!';
-var EXPORT_ERROR = 'Some errors occurred...';
-var EXPORT_NODATA = 'No data found for your account';
-var EXPORT_UPLOADED = 'Uploaded successfully';
+var PROPERTY_WNDSCR = 'Keverési panel stílusa';
+var PROPERTY_WNDSTAT = 'Statisztikai panel stílusa';
+var PROPERTY_WNDTOOL = 'Eszközök panel stílusa';
+var PROPERTY_WND_STR = 'Normál|Lapos';
+var EXPORT_DATAEXPORT = 'Adatok importálása/exportálása';
+var EXPORT_TOFILE = 'Exportálás file-ba';
+var EXPORT_FROMFILE = 'Importálás file-ból';
+var EXPORT_TOSERV = 'Exportálás a szerverre';
+var EXPORT_FROMSERV = 'Importálás a szerverről';
+var EXPORT_FROMOTHER = 'Szakasz(ok) importálása más időmérőkből';
+var EXPORT_USERID = 'Kérlek, add meg a fiókod (csak betűk és számok)';
+var EXPORT_INVID = 'Csak betűk és számok megengedettek!';
+var EXPORT_ERROR = 'Néhány probléma lépett fel...';
+var EXPORT_NODATA = 'Nem található adat a fiókodon';
+var EXPORT_UPLOADED = 'Sikeresen feltöltve';
 var EXPORT_CODEPROMPT = 'Save this code, or type saved code to import';
 var EXPORT_ONLYOPT = 'Export/Import only Options';
 var EXPORT_LOGINGGL = 'Login Using Google Account';
@@ -51,59 +51,59 @@ var TOOLS_SELECTFUNC = 'Funkció';
 var TOOLS_CROSS = 'kereszt megoldása';
 var TOOLS_EOLINE = 'EOLine megoldása';
 var TOOLS_ROUX1 = 'Roux S1 megoldása';
-var TOOLS_GIIKER = 'Giiker Cube';
+var TOOLS_GIIKER = 'Giiker kocka';
 var TOOLS_IMAGE = 'Keverés mutatása';
 var TOOLS_STATS = 'Statisztikák';
-var TOOLS_HUGESTATS = 'cross-session stats';
+var TOOLS_HUGESTATS = 'több szakasz statisztikái';
 var TOOLS_DISTRIBUTION = 'idő megoszlása';
-var TOOLS_TREND = 'time trend';
-var TOOLS_METRONOME = 'metronome';
-var TOOLS_CFMTIME = 'Confirm time';
+var TOOLS_TREND = 'idő trend';
+var TOOLS_METRONOME = 'metronóm';
+var TOOLS_CFMTIME = 'Idő megerősítése';
 var PROPERTY_IMGSIZE = 'Keverési kép mérete';
 var TIMER_INSPECT = 'memorizációs idő';
 var TIMER_SOLVE = 'megoldás';
-var PROPERTY_USEMOUSE = 'use mouse timer';
+var PROPERTY_USEMOUSE = 'egér használata időmérőként';
 var PROPERTY_TIMEU = 'időmérő beállítása';
-var PROPERTY_TIMEU_STR = 'update|0.1s|seconds|inspection|none';
+var PROPERTY_TIMEU_STR = 'frissítés|0.1s|másodpercek|megnézés|nincs';
 var PROPERTY_PRETIME = 'a szóköz lenttartásának az ideje (másodpercben)';
 var PROPERTY_ENTERING = 'eredmény bevitele';
 var PROPERTY_ENTERING_STR = 'időmérővel|gépeléssel|stackmattel|MoYuTimer|virtuálisan|Giiker';
 var PROPERTY_COLOR = 'színséma választása';
-var PROPERTY_COLORS = 'font color|background color|board color|button color|link color|Logo color|Logo bgcolor';
-var PROPERTY_VIEW = 'UI style is';
-var PROPERTY_VIEW_STR = 'Auto|Mobile|Desktop';
+var PROPERTY_COLORS = 'betűszín|háttérszín|tábla színe|gomb színe|link színe|logó színe|logó háttérszíne';
+var PROPERTY_VIEW = 'A felület stílusa';
+var PROPERTY_VIEW_STR = 'Automatikus|Mobil|Asztali';
 var PROPERTY_UIDESIGN = 'UI design is';
 var PROPERTY_UIDESIGN_STR = 'Normal|Material design|Normal w/o shadows';
-var COLOR_EXPORT = 'Please save the string for import';
-var COLOR_IMPORT = 'Please input the string exported';
-var COLOR_FAIL = 'Incorrect Data, Import Failed';
-var PROPERTY_FONTCOLOR_STR = 'black|white';
+var COLOR_EXPORT = 'Kérlek, mentsd el a szöveget importáláshoz!';
+var COLOR_IMPORT = 'Kérlek, add meg az exportált szöveget!';
+var COLOR_FAIL = 'Helytelen adatok, importálás sikertelen';
+var PROPERTY_FONTCOLOR_STR = 'fekete|fehér';
 var PROPERTY_COLOR_STR = 'random|1-es stílus|2-es stílus|3-as stílus|fekete|fehér|6-os stílus|manual|import/export...';
 var PROPERTY_FONT = 'időmérő betűtípusa';
 var PROPERTY_FONT_STR = 'random digitális|normál|1-es digitális|2-es digitális|3-as digitális|4-es digitális|5-ös digitális';
 var PROPERTY_FORMAT = 'idő formátum';
 var PROPERTY_USEKSC = 'billentyűparancsok használata';
 var PROPERTY_NTOOLS = 'eszközök száma';
-var PROPERTY_AHIDE = 'Hide All Elements When Timing';
+var PROPERTY_AHIDE = 'Összes elem elrejtése mérés közben';
 var SCRAMBLE_LAST = 'utolsó';
 var SCRAMBLE_NEXT = 'következő';
 var SCRAMBLE_SCRAMBLE = ' keverés';
 var SCRAMBLE_LENGTH = 'hosszúság';
 var SCRAMBLE_INPUT = 'keverések bevitele';
-var PROPERTY_VRCSPEED = 'VRC base speed (tps)';
-var PROPERTY_VRCMP = 'multi-phase';
-var PROPERTY_VRCMPS = 'None|CFOP|CF+OP|CFFFFOP|Roux';
-var PROPERTY_GIIKERVRC = 'Show virtual Giiker cube';
-var PROPERTY_GIISOK_DELAY = 'Mark scrambled if stay';
-var PROPERTY_GIISOK_DELAYS = '2s|3s|4s|5s|Never|Correctly scrambled';
-var PROPERTY_GIISOK_KEY = 'Mark scrambled with spacebar';
-var PROPERTY_GIISOK_MOVE = 'Mark scrambled by doing';
-var PROPERTY_GIISOK_MOVES = 'U4, R4, etc|(U U\')2, (U\' U)2, etc|Never';
-var PROPERTY_GIISBEEP = 'Beep when mark scrambled';
-var PROPERTY_GIIRST = 'Reset Giiker cube when connect';
-var PROPERTY_GIIRSTS = 'Always|Prompt|Never';
-var CONFIRM_GIIRST = 'Reset Giiker cube as solved?';
-var PROPERTY_GIIAED = 'Auto hardware error detection';
+var PROPERTY_VRCSPEED = 'VRC alap sebessége (tps)';
+var PROPERTY_VRCMP = 'többfázisú';
+var PROPERTY_VRCMPS = 'Nincs|CFOP|CF+OP|CFFFFOP|Roux';
+var PROPERTY_GIIKERVRC = 'Virtuális Giiker kocka mutatása';
+var PROPERTY_GIISOK_DELAY = 'Megjelőlés kevertként ekkor:';
+var PROPERTY_GIISOK_DELAYS = '2s|3s|4s|5s|Soha|Helyes keverésnél';
+var PROPERTY_GIISOK_KEY = 'Megjelőlés kevertként szóközzel';
+var PROPERTY_GIISOK_MOVE = 'Megjelelés keverként a következővel';
+var PROPERTY_GIISOK_MOVES = 'U4, R4, stb|(U U\')2, (U\' U)2, stb|Soha';
+var PROPERTY_GIISBEEP = 'Hangjelzés kevertnek jelöléskor';
+var PROPERTY_GIIRST = 'Giiker kocka visszaállítása csatlakozáskor';
+var PROPERTY_GIIRSTS = 'mindig|rákérdezés|soha';
+var CONFIRM_GIIRST = 'Giiker kocka visszaállítása kirakottra?';
+var PROPERTY_GIIAED = 'Automatikus hardver hiba érzékelés';
 var scrdata = [
 	['WCA', [
 		['3x3x3', "333", 0],
@@ -113,19 +113,19 @@ var scrdata = [
 		['6x6x6', "666wca", -80],
 		['7x7x7', "777wca", -100],
 		['3x3 bld (vakon)', "333ni", 0],
-		['3x3 fm', "333fm", 0],
+		['3x3 FM', "333fm", 0],
 		['3x3 oh (egykezes)', "333oh", 0],
 		['3x3 ft (lábbal)', "333ft", 0],
-		['clock', "clkwca", 0],
+		['óra', "clkwca", 0],
 		['megaminx', "mgmp", -70],
 		['pyraminx', "pyrso", -10],
 		['skewb', "skbso", 0],
 		['sq1', "sqrs", 0],
-		['4x4 bld', "444bld", -40],
-		['5x5 bld', "555bld", -60],
-		['3x3 mbld', "r3ni", 5]
+		['4x4 BLD', "444bld", -40],
+		['5x5 BLD', "555bld", -60],
+		['3x3 MBLD', "r3ni", 5]
 	]],
-	['Input', [
+	['Bevitel', [
 		['外部', "input", 0]
 	]],
 	['===WCA===', [
@@ -133,30 +133,30 @@ var scrdata = [
 	]],
 	['3x3x3', [
 		["WCA", "333", 0],
-		['old style', "333o", 25],
-		['3x3x3 for noobs', "333noob", 25],
-		['edges only', "edges", 0],
-		['corners only', "corners", 0],
-		['last layer', "ll", 0],
-		['zb last layer', "zbll", 0],
-		['corners of last layer', "cll", 0],
-		['edges of last layer', "ell", 0],
-		['last six edges', "lse", 0],
-		['last six edges&ltM,U&gt', "lsemu", 0],
+		['régi stílus', "333o", 25],
+		['3x3x3 kezdőknek', "333noob", 25],
+		['csak élek', "edges", 0],
+		['csak sarkok', "corners", 0],
+		['utolsó réteg', "ll", 0],
+		['ZB utolsó réteg', "zbll", 0],
+		['utolsó réteg sarkai', "cll", 0],
+		['utolsó réteg élei', "ell", 0],
+		['utolsó hat él', "lse", 0],
+		['utolsó hat él&ltM,U&gt', "lsemu", 0],
 		['Roux L10P', "cmll", 0],
-		['cross solved', "f2l", 0],
-		['last slot + last layer', "lsll2", 0],
+		['kereszt megoldva', "f2l", 0],
+		['utolsó pár + utolsó réteg', "lsll2", 0],
 		['2GLL', "2gll", 0],
 		['ZBLS', "zbls", 0],
 		['ZZLL', "zzll", 0],
 		['OLL', "oll", 0],
 		['PLL', "pll", 0],
 		['EOLine', "eoline", 0],
-		['easy cross', "easyc", 3]
+		['könnyű kereszt', "easyc", 3]
 	]],
 	['2x2x2', [
 		["WCA", "222so", 0],
-		['optimal random state', "222o", 0],
+		['optimális véletlen állapot', "222o", 0],
 		['3-gen', "2223", 25],
 		['EG', "222eg", 0],
 		['EG0', "222eg0", 0],
@@ -165,69 +165,69 @@ var scrdata = [
 	]],
 	['4x4x4', [
 		["WCA", "444wca", -40],
-		['random move', "444m", 40],
+		['véletlen mozdulatok', "444m", 40],
 		['SiGN', "444", 40],
 		['YJ', "444yj", 40],
-		['4x4x4 edges', "4edge", 8],
+		['4x4x4 élek', "4edge", 8],
 		['R,r,U,u', "RrUu", 40]
 	]],
 	['5x5x5', [
 		["WCA", "555wca", 60],
 		['SiGN', "555", 60],
-		['5x5x5 edges', "5edge", 8]
+		['5x5x5 élek', "5edge", 8]
 	]],
 	['6x6x6', [
 		["WCA", "666wca", 80],
 		['SiGN', "666si", 80],
-		['prefix', "666p", 80],
-		['suffix', "666s", 80],
-		['6x6x6 edges', "6edge", 8]
+		['előtag', "666p", 80],
+		['utótag', "666s", 80],
+		['6x6x6 élek', "6edge", 8]
 	]],
 	['7x7x7', [
 		["WCA", "777wca", 100],
 		['SiGN', "777si", 100],
-		['prefix', "777p", 100],
-		['suffix', "777s", 100],
-		['7x7x7 edges', "7edge", 8]
+		['előtag', "777p", 100],
+		['utótag', "777s", 100],
+		['7x7x7 élek', "7edge", 8]
 	]],
-	['Clock', [
+	['Óra', [
 		['jaap', "clk", 0],
 		['wca', "clkwca", 0],
-		['optimal', "clko", 0],
-		['concise', "clkc", 0],
-		['efficient pin order', "clke", 0]
+		['optimális', "clko", 0],
+		['tömör', "clkc", 0],
+		['hatékony pöcök sorrend', "clke", 0]
 	]],
 	['Megaminx', [
 		["WCA", "mgmp", 70],
-		['Carrot', "mgmc", 70],
-		['old style', "mgmo", 70]
+		['Répa', "mgmc", 70],
+		['régi stílus', "mgmo", 70]
 	]],
 	['Pyraminx', [
 		["WCA", "pyrso", 10],
-		['optimal random state', "pyro", 0],
-		['random moves', "pyrm", 25]
+		['omtimális véletlen állapot', "pyro", 0],
+		['véletlen mozdulatok', "pyrm", 25]
 	]],
 	['Skewb', [
 		["WCA", "skbso", 0],
-		['U L R B', "skb", 25]
+		['U L B R', "skb", 25]
 	]],
 	['Square-1', [
 		["WCA", "sqrs", 0],
 		["CSP", "sqrcsp", 0],
-		['face turn metric', "sq1h", 40],
-		['twist metric', "sq1t", 20]
+		['oldal fordítási rendszer', "sq1h", 40],
+		['tekerési rendszer', "sq1t", 20]
 	]],
-	['===OTHER===', [
+	['===EGYÉB===', [
 		['--', "blank", 0]
 	]],
 	['15 puzzle', [
-		['piece moves', "15p", 80],
-		['blank moves', "15pm", 80]
+		['elem mozgások', "15p", 80],
+		['üres mozgások', "15pm", 80]
 	]],
 	['LxMxN', [
 		['1x3x3 (Floppy Cube)', "133", 0],
-		['2x2x3 (Tower Cube)', "223", 0],
-		['2x3x3 (Domino)', "233", 25],
+		['2x2x3 (Torony kocka)', "223", 0],
+		['2x3x3 (Dominó)', "233", 25],
 		['3x3x4', "334", 40],
 		['3x3x5', "335", 25],
 		['3x3x6', "336", 40],
@@ -307,10 +307,10 @@ var scrdata = [
 		['lots of 3x3x3s', "r3", 5],
 		['234 relay', "r234", 0],
 		['2345 relay', "r2345", 0],
-		['23456 relay', "r23456", 0],
-		['234567 relay', "r234567", 0]
+		['23456 váltó', "r23456", 0],
+		['234567 váltó', "r234567", 0]
 	]],
-	['===JOKES===', [
+	['===POÉN===', [
 		['--', "blank", 0]
 	]],
 	['1x1x1', [
@@ -330,35 +330,35 @@ var scrdata = [
 	]]
 ];
 var SCRAMBLE_NOOBST = [
-	['turn the top face', 'turn the bottom face'],
-	['turn the right face', 'turn the left face'],
-	['turn the front face', 'turn the back face']
+	['fordítsd a felső oldalt', 'fordítsd az alsó oldalt'],
+	['fordítsd a jobb oldalt', 'fordítsd a bal oldalt'],
+	['fordítsd az első oldalt', 'fordítsd a hátsó oldalt']
 ];
-var SCRAMBLE_NOOBSS = ' clockwise by 90 degrees,| counterclockwise by 90 degrees,| by 180 degrees,';
+var SCRAMBLE_NOOBSS = ' 90 fokkal óramutató járásval megegyezően,| 90 fokkal óramutató járásával ellenkezően,| 180 fokot,';
 var STATS_CFM_RESET = 'Időszak eredményeinek törlése??';
-var STATS_CFM_DELSS = 'delete session [%s]?';
-var STATS_CFM_DELMUL = 'The Number Of Deleted Values From Current Index?';
+var STATS_CFM_DELSS = 'Törlöd [%s] időszakot?';
+var STATS_CFM_DELMUL = 'Hány értéket szeretnél törölni a jelenlegi időszakból?';
 var STATS_CFM_DELETE = 'Eredmény törlése?';
 var STATS_COMMENT = 'Megjegyzés';
-var STATS_DATE = 'Date';
+var STATS_DATE = 'Dátum';
 var STATS_CURROUND = 'Jelen forduló statisztikái';
 var STATS_CURSESSION = 'Jelen időszak statisztikái';
-var STATS_CURSPLIT = 'Phase %d of Current Session Statistics';
-var STATS_EXPORTCSV = 'Export CSV';
-var STATS_SSMGR_TITLE = 'Session Manager';
-var STATS_SSMGR_NAME = 'Name';
-var STATS_SSMGR_DETAIL = 'Session Details';
-var STATS_SSMGR_OPS = 'Rename|Create|Split|Merge|Delete';
-var STATS_SSMGR_ORDER = 'Order by scramble';
-var STATS_SSMGR_ODCFM = 'Sort all sessions by scramble?';
-var STATS_ALERTMG = 'Merge all times in session [%f] to the end of session [%t]?';
-var STATS_PROMPTSPL = 'Number of latest times split from session [%s]?';
+var STATS_CURSPLIT = 'A jelenlegi időszak %d fázisának statisztikái';
+var STATS_EXPORTCSV = 'CSV exportálása';
+var STATS_SSMGR_TITLE = 'Időszak kezelő';
+var STATS_SSMGR_NAME = 'Név';
+var STATS_SSMGR_DETAIL = 'Időszak részletek';
+var STATS_SSMGR_OPS = 'Átnvezés|Létrehozás|Szétválasztás|Egybevonás|Törlés';
+var STATS_SSMGR_ORDER = 'Rendezés keverés szerint';
+var STATS_SSMGR_ODCFM = 'Az összes időszakot rendezed keverés szerint?';
+var STATS_ALERTMG = 'Egybevonod az összes időt a(z) [%f] időszakban a(z) [%t] időszak végéig?';
+var STATS_PROMPTSPL = 'Hány időt szeretnél leválasztani a(z) [%s] időszakról?';
 var STATS_ALERTSPL = 'Should split or leave 1 time at least';
 var STATS_AVG = 'átlag';
 var STATS_SOLVE = 'megoldás';
 var STATS_TIME = 'eredmény';
 var STATS_SESSION = 'Időszak';
-var STATS_SESSION_NAME = 'Edit session name';
+var STATS_SESSION_NAME = 'Időszak átnevezése';
 var STATS_SESSION_NAMEC = 'Name of the new session';
 var STATS_STRING = 'legjobb|pillanatnyi|legrosszabb|csTimer által generálva  %év-%hó-%napján|megoldások/összes: %d|egyedüli|mean of %mk|avg of %mk|Átlag: %v{ (σ = %sgm)}|Mean: %v|Idő lista:|solving from %s to %e|Totally spent: %d';
 var STATS_PREC = 'Idő kijelzés pontossága';
