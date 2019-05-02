9bd56990f5aeb1d566dfe7715e5e7dd89a4046fd
chenshuang <cs0x7f@gmail.com>
2019-05-02 19:17:30 +0800

    add data log

--- dist/userdata.php
+++ dist/userdata.php
@@ -1,5 +1,6 @@
 <?php
 	define('CSTIMER_PREFIX', 'prefix');
+	define('CSTIMER_USERDATA_LOGFILE', 'logfile');
 
 	if (!isset($_POST['id']) || empty($_POST['id']) || strlen($_POST['id']) >= 250) {
 		echo '{"retcode":400}';
@@ -20,6 +21,7 @@
 			echo '{"retcode":400}';
 			exit(0);
 		}
+		error_log("[" . date("Y-m-d H:i:sO") . "] SET " . $_POST['id'] . " " . strlen($_POST['data']) . "\n", 3, CSTIMER_USERDATA_LOGFILE);
 		$result = $redis->set(CSTIMER_PREFIX . $_POST['id'], $_POST['data']);
 		if ($result === true) {
 			echo '{"retcode":0}';
@@ -27,6 +29,7 @@
 			echo '{"retcode":500}';
 		}
 	} else {//GET
+		error_log("[" . date("Y-m-d H:i:sO") . "] GET " . $_POST['id'] . strlen($_POST['data']) . "\n", 3, CSTIMER_USERDATA_LOGFILE);
 		$ret = $redis->get(CSTIMER_PREFIX . $_POST['id']);
 		if ($ret === false) {
 			echo '{"retcode":404}';
