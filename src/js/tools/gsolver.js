1b3d55abefdff4b65c22f102e86dba9c45cd3867
chenshuang <cs0x7f@gmail.com>
2020-01-01 21:38:28 +0800

    clean up

--- src/js/tools/gsolver.js
+++ src/js/tools/gsolver.js
@@ -1,6 +1,6 @@
 "use strict";
 
-(function() {
+var gsolver = (function() {
 
 	var curScramble;
 	var curScrambleStr;
@@ -168,12 +168,30 @@
 			[[D1, D3, D9, D7], [D2, D6, D8, D4], [F7, R7, B7, L7], [F8, R8, B8, L8], [F9, R9, B9, L9]], // D
 			[[L1, L3, L9, L7], [L2, L6, L8, L4], [U1, F1, D1, B9], [U4, F4, D4, B6], [U7, F7, D7, B3]], // L
 			[[B1, B3, B9, B7], [B2, B6, B8, B4], [U3, L1, D7, R9], [U2, L4, D8, R6], [U1, L7, D9, R3]], // B
+			[[U1, U3, U9, U7], [U2, U6, U8, U4], [F1, L1, B1, R1], [F2, L2, B2, R2], [F3, L3, B3, R3],
+			 [F4, L4, B4, R4], [F5, L5, B5, R5], [F6, L6, B6, R6]], // u = Uw
+			[[R1, R3, R9, R7], [R2, R6, R8, R4], [U3, B7, D3, F3], [U6, B4, D6, F6], [U9, B1, D9, F9],
+			 [U2, B8, D2, F2], [U5, B5, D5, F5], [U8, B2, D8, F8]], // r = Rw
+			[[F1, F3, F9, F7], [F2, F6, F8, F4], [U7, R1, D3, L9], [U8, R4, D2, L6], [U9, R7, D1, L3],
+			 [U4, R2, D6, L8], [U5, R5, D5, L5], [U6, R8, D4, L2]], // f = Fw
+			[[D1, D3, D9, D7], [D2, D6, D8, D4], [F7, R7, B7, L7], [F8, R8, B8, L8], [F9, R9, B9, L9],
+			 [F4, R4, B4, L4], [F5, R5, B5, L5], [F6, R6, B6, L6]], // d = Dw
+			[[L1, L3, L9, L7], [L2, L6, L8, L4], [U1, F1, D1, B9], [U4, F4, D4, B6], [U7, F7, D7, B3],
+			 [U2, F2, D2, B8], [U5, F5, D5, B5], [U8, F8, D8, B2]], // l = Lw
+			[[B1, B3, B9, B7], [B2, B6, B8, B4], [U3, L1, D7, R9], [U2, L4, D8, R6], [U1, L7, D9, R3],
+			 [U6, L2, D4, R8], [U5, L5, D5, R5], [U4, L8, D6, R2]], // b = Bw
 			[[U2, F2, D2, B8], [U5, F5, D5, B5], [U8, F8, D8, B2]], // M
+			[[F4, R4, B4, L4], [F5, R5, B5, L5], [F6, R6, B6, L6]], // E
+			[[U4, R2, D6, L8], [U5, R5, D5, L5], [U6, R8, D4, L2]], // S
 			[[R1, R3, R9, R7], [R2, R6, R8, R4], [U3, B7, D3, F3], [U6, B4, D6, F6], [U9, B1, D9, F9],
 			 [L1, L7, L9, L3], [L2, L4, L8, L6], [U1, B9, D1, F1], [U4, B6, D4, F4], [U7, B3, D7, F7],
 			 [U2, B8, D2, F2], [U5, B5, D5, F5], [U8, B2, D8, F8]], // x
-			[[R1, R3, R9, R7], [R2, R6, R8, R4], [U3, B7, D3, F3], [U6, B4, D6, F6], [U9, B1, D9, F9],
-			 [U2, B8, D2, F2], [U5, B5, D5, F5], [U8, B2, D8, F8]] // r = Rw
+			[[U1, U3, U9, U7], [U2, U6, U8, U4], [F1, L1, B1, R1], [F2, L2, B2, R2], [F3, L3, B3, R3],
+			 [D1, D7, D9, D3], [D2, D4, D8, D6], [F7, L7, B7, R7], [F8, L8, B8, R8], [F9, L9, B9, R9],
+			 [F4, L4, B4, R4], [F5, L5, B5, R5], [F6, L6, B6, R6]], // y
+			[[F1, F3, F9, F7], [F2, F6, F8, F4], [U7, R1, D3, L9], [U8, R4, D2, L6], [U9, R7, D1, L3],
+			 [B1, B7, B9, B3], [B2, B4, B8, B6], [U3, R9, D7, L1], [U2, R6, D8, L4], [U1, R3, D9, L7],
+			 [U4, R2, D6, L8], [U5, R5, D5, L5], [U6, R8, D4, L2]] // z
 		];
 
 		var moves = appendSuffix({
@@ -208,7 +226,7 @@
 
 		function cubeMove(state, move) {
 			var ret = state.split('');
-			var swaps = moveData["URFDLBMxr".indexOf(move[0])];
+			var swaps = moveData["URFDLBurfdlbMESxyz".indexOf(move[0])];
 			var pow = "? 2'".indexOf(move[1]);
 			for (var i = 0; i < swaps.length; i++) {
 				mathlib.acycle(ret, swaps[i], pow);
@@ -417,7 +435,10 @@
 			}
 		}
 
-		return exec333StepSolver;
+		return {
+			exec: exec333StepSolver,
+			move: cubeMove
+		};
 	})();
 
 	var sq1Cube = (function() {
@@ -912,7 +933,7 @@
 			if (type == '222face' && tools.isPuzzle('222')) {
 				pocketCube(scramble[1], span);
 			} else if (type.startsWith('333') && tools.isPuzzle('333') && /^[URFDLB 2']+$/.exec(scramble[1])) {
-				rubiksCube(type.slice(3), scramble[1], span);
+				rubiksCube.exec(type.slice(3), scramble[1], span);
 			} else if (type == 'sq1cs' && tools.isPuzzle('sq1')) {
 				sq1Cube(scramble[1], span);
 			} else if (type == 'skbl1' && tools.isPuzzle('skb')) {
@@ -938,4 +959,8 @@
 			tools.regTool('skbl1', TOOLS_SOLVERS + '>Skewb Face', execFunc.bind(null, 'skbl1'));
 		});
 	});
+
+	return {
+		rubiksCube: rubiksCube
+	}
 })();
