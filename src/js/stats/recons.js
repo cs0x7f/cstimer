654e41efc06f7d31e133fb849e022446ca1c078d
cs0x7f <cs0x7f@gmail.com>
2023-03-12 16:27:09 +0800

    clean up reconstruction

--- src/js/stats/recons.js
+++ src/js/stats/recons.js
@@ -5,7 +5,7 @@ var recons = execMain(function() {
 	var table = $('<table class="table">');
 	var rangeSelect = $('<select>');
 	var methodSelect = $('<select>');
-	var requestBack = $('<span class="click">Back</span>');
+	var requestBack = $('<span class="click" />');
 	var tableTh = $('<tr>').append($('<th style="padding:0;">').append(methodSelect), '<th>insp</th><th>exec</th><th>turn</th><th>fps</th>');
 
 	function doMove(c, d, moveStr, isInv) {
@@ -72,10 +72,10 @@ var recons = execMain(function() {
 	}
 
 	function calcRecons(times, method) {
-		var solution = times[4];
-		if (!solution) {
+		if (!times || !times[4] || times[0][0] < 0) {
 			return;
 		}
+		var solution = times[4];
 		var c = new mathlib.CubieCube();
 		var d = new mathlib.CubieCube();
 		c.ori = 0;
@@ -125,7 +125,7 @@ var recons = execMain(function() {
 	}
 
 	// data = [[name, insp, exec, turn], ...]
-	function renderResult(stepData, isRequest) {
+	function renderResult(stepData, tidx) {
 		var maxSubt = 0;
 		for (var i = 0; i < stepData.length; i++) {
 			maxSubt = Math.max(stepData[i][1] + stepData[i][2], maxSubt);
@@ -149,13 +149,27 @@ var recons = execMain(function() {
 				'<td style="padding-bottom:0;padding-top:0;">' + (val[3] > 0 && val[1] + val[2] > 0 ? Math.round(val[3] / (val[1] + val[2]) * 10000 ) / 10 : 'N/A') + '</td>' +
 				'</tr>');
 		}
-		var endTr = $('<tr>').append(isRequest ? $('<td>').append(requestBack) : $('<td style="padding:0;">').append(rangeSelect),
+		var endTr = $('<tr>').append(tidx ? $('<td>').append(requestBack) : $('<td style="padding:0;">').append(rangeSelect),
 			'<td>' + kernel.pretty(totIns) + '</td>' +
 			'<td>' + kernel.pretty(totExec) + '</td>' +
 			'<td>' + Math.round(totMov * 10) / 10 + '</td>' +
 			'<td>' + (totMov > 0 && totIns + totExec > 0 ? Math.round(totMov / (totIns + totExec) * 10000 ) / 10 : 'N/A') + '</td>');
+		table.empty().append(tableTh);
 		tableTh.after(str.join(''), endTr);
 		rangeSelect.unbind('change').change(procClick);
+		methodSelect.unbind('change').change(procClick);
+		requestBack.text('No.' + tidx).unbind('click').click(procClick);
+	}
+
+	function renderEmpty(isRequest) {
+		table.empty().append(tableTh);
+		tableTh.after(
+			$('<tr>').append(
+				isRequest ? $('<td>').append(requestBack) : $('<td style="padding:0;">').append(rangeSelect),
+				'<td colspan=4>' + TOOLS_RECONS_NODATA + '</td>')
+		);
+		rangeSelect.unbind('change').change(procClick);
+		methodSelect.unbind('change').change(procClick);
 		requestBack.unbind('click').click(procClick);
 	}
 
@@ -171,28 +185,23 @@ var recons = execMain(function() {
 	}
 
 	function reqRecons(signal, value) {
-		table.empty().append(tableTh);
-		methodSelect.unbind('change').change(procClick);
+		var method = methodSelect.val();
 		var times = value[0];
-		if (!times) {
-			tableTh.after('<tr><td colspan=5>No Solution Valid!</td></tr>');
+		var data = calcRecons(times, method);
+		if (!data) {
+			renderEmpty(true);
 			return;
 		}
-		var num = value[1];
-		var method = methodSelect.val();
-		var data = calcRecons(times, method);
 		var steps = cubeutil.getStepNames(method);
 		var stepData = [];
 		for (var i = steps.length - 1; i >= 0; i--) {
 			var curData = data[i] || [0, 0, 0, 0];
 			stepData.push([steps[i], curData[1] - curData[0], curData[2] - curData[1], curData[3]]);
 		}
-		renderResult(stepData, true);
+		renderResult(stepData, value[1] + 1);
 	}
 
 	function update() {
-		table.empty().append(tableTh);
-		methodSelect.unbind('change').change(procClick);
 		var nsolv = stats.getTimesStatsList().timesLen;
 		var nrec = rangeSelect.val();
 		if (nrec == 'single') {
@@ -205,7 +214,7 @@ var recons = execMain(function() {
 			nrec = nsolv;
 		}
 		if (!nrec) {
-			tableTh.after('<tr><td colspan=5>No Solution Valid!</td></tr>');
+			renderEmpty(false);
 			return;
 		}
 		var method = methodSelect.val();
@@ -214,11 +223,11 @@ var recons = execMain(function() {
 		var stepData = [];
 		for (var s = nsolv - 1; s >= nsolv - nrec; s--) {
 			var times = stats.timesAt(s);
-			if (!times || !times[4]) {
+			var data = calcRecons(times, method);
+			if (!data) {
 				continue;
 			}
 			nvalid++;
-			var data = calcRecons(times, method);
 			for (var i = steps.length - 1; i >= 0; i--) {
 				var curData = data[i] || [0, 0, 0, 0];
 				var sidx = steps.length - i - 1;
@@ -229,7 +238,7 @@ var recons = execMain(function() {
 			}
 		}
 		if (nvalid == 0) {
-			tableTh.after('<tr><td colspan=5>No Solution Valid!</td></tr>');
+			renderEmpty(false);
 			return;
 		}
 		for (var i = 0; i < steps.length; i++) {
@@ -246,7 +255,7 @@ var recons = execMain(function() {
 
 	$(function() {
 		if (typeof tools != "undefined") {
-			tools.regTool('recons', 'Reconstruction', execFunc);
+			tools.regTool('recons', TOOLS_RECONS, execFunc);
 		}
 		stats.regUtil('recons', update);
 		kernel.regListener('recons', 'reqrec', reqRecons);
