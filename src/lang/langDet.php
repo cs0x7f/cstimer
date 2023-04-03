65c7c1d7c31765c6069a7251793f6cec7e531f5b
Andy Fedotov <andy.fedotov@gmail.com>
2023-04-03 18:34:11 +0300

    Better versioning for builds

--- src/lang/langDet.php
+++ src/lang/langDet.php
@@ -1,5 +1,5 @@
 <?php
-$version = "2023.03.31";
+$version = "@@VERSION@@";
 
 function prefered_language($available_languages, $req_lang) {
   if ($req_lang == "auto" && isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
