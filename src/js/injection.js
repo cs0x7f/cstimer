85d4a8aac3a8aac9e8a7d78646b691e975ffc4c9
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-15 19:57:07 +0200

    adjust injectSolveListener callback - reconstruction

--- src/js/injection.js
+++ src/js/injection.js
@@ -1,7 +1,10 @@
 // also see "window.injectScrambles"
 
 window.injectSolveListener = function (callback) {
-  kernel.regListener("injection", "time", function () {
-    callback(...arguments);
+  kernel.regListener("injection", "time", (...args) => {
+    const reconstruction = args[1][4][0]
+    callback({ reconstruction });
+    // callback(args);
+    // console.log(args)
   });
 };
