17925cf7a743e0c7173de1043a5d3a0dd506f907
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-27 20:32:56 +0200

    remove regex which was causing bug with S E M moves reconstructions

--- src/vscubing/integration.js
+++ src/vscubing/integration.js
@@ -8,9 +8,10 @@ const api = (function () {
       }
 
       const csReconstruction = time[4][0];
-      const algCubingReconstruction = csReconstruction
-        .replace(/@(\d+)/g, "/*$1*/")
-        .replace(/-/g, "&#45;");
+      const algCubingReconstruction = csReconstruction.replace(
+        /@(\d+)/g,
+        "/*$1*/",
+      );
 
       callback({
         reconstruction: algCubingReconstruction,
@@ -29,8 +30,11 @@ const api = (function () {
   }
 
   function hackForFreshLocalStorage() {
-    localStorage.setItem('properties', `{\"sessionData\":\"{\\\"1\\\":{\\\"name\\\":1,\\\"opt\\\":{},\\\"rank\\\":1,\\\"stat\\\":[0,0,-1],\\\"date\\\":[null,null]},\\\"2\\\":{\\\"name\\\":2,\\\"opt\\\":{},\\\"rank\\\":2},\\\"3\\\":{\\\"name\\\":3,\\\"opt\\\":{},\\\"rank\\\":3},\\\"4\\\":{\\\"name\\\":4,\\\"opt\\\":{},\\\"rank\\\":4},\\\"5\\\":{\\\"name\\\":5,\\\"opt\\\":{},\\\"rank\\\":5},\\\"6\\\":{\\\"name\\\":6,\\\"opt\\\":{},\\\"rank\\\":6},\\\"7\\\":{\\\"name\\\":7,\\\"opt\\\":{},\\\"rank\\\":7},\\\"8\\\":{\\\"name\\\":8,\\\"opt\\\":{},\\\"rank\\\":8},\\\"9\\\":{\\\"name\\\":9,\\\"opt\\\":{},\\\"rank\\\":9},\\\"10\\\":{\\\"name\\\":10,\\\"opt\\\":{},\\\"rank\\\":10},\\\"11\\\":{\\\"name\\\":11,\\\"opt\\\":{},\\\"rank\\\":11},\\\"12\\\":{\\\"name\\\":12,\\\"opt\\\":{},\\\"rank\\\":12},\\\"13\\\":{\\\"name\\\":13,\\\"opt\\\":{},\\\"rank\\\":13},\\\"14\\\":{\\\"name\\\":14,\\\"opt\\\":{},\\\"rank\\\":14},\\\"15\\\":{\\\"name\\\":15,\\\"opt\\\":{},\\\"rank\\\":15}}\",\"input\":\"v\"}`)
-    localStorage.removeItem('cachedScr')
+    localStorage.setItem(
+      "properties",
+      `{\"sessionData\":\"{\\\"1\\\":{\\\"name\\\":1,\\\"opt\\\":{},\\\"rank\\\":1,\\\"stat\\\":[0,0,-1],\\\"date\\\":[null,null]},\\\"2\\\":{\\\"name\\\":2,\\\"opt\\\":{},\\\"rank\\\":2},\\\"3\\\":{\\\"name\\\":3,\\\"opt\\\":{},\\\"rank\\\":3},\\\"4\\\":{\\\"name\\\":4,\\\"opt\\\":{},\\\"rank\\\":4},\\\"5\\\":{\\\"name\\\":5,\\\"opt\\\":{},\\\"rank\\\":5},\\\"6\\\":{\\\"name\\\":6,\\\"opt\\\":{},\\\"rank\\\":6},\\\"7\\\":{\\\"name\\\":7,\\\"opt\\\":{},\\\"rank\\\":7},\\\"8\\\":{\\\"name\\\":8,\\\"opt\\\":{},\\\"rank\\\":8},\\\"9\\\":{\\\"name\\\":9,\\\"opt\\\":{},\\\"rank\\\":9},\\\"10\\\":{\\\"name\\\":10,\\\"opt\\\":{},\\\"rank\\\":10},\\\"11\\\":{\\\"name\\\":11,\\\"opt\\\":{},\\\"rank\\\":11},\\\"12\\\":{\\\"name\\\":12,\\\"opt\\\":{},\\\"rank\\\":12},\\\"13\\\":{\\\"name\\\":13,\\\"opt\\\":{},\\\"rank\\\":13},\\\"14\\\":{\\\"name\\\":14,\\\"opt\\\":{},\\\"rank\\\":14},\\\"15\\\":{\\\"name\\\":15,\\\"opt\\\":{},\\\"rank\\\":15}}\",\"input\":\"v\"}`,
+    );
+    localStorage.removeItem("cachedScr");
   }
 
   function setInputModeToVirtual() {
@@ -46,12 +50,14 @@ const api = (function () {
 })();
 const POST_MESSAGE_SOURCE = "vs-solver-integration";
 
-api.hackForFreshLocalStorage()
+api.hackForFreshLocalStorage();
 window.addEventListener("load", () => {
   api.setInputModeToVirtual();
+  api.importScramble("R");
 });
 
 api.regSolvesListener((result) => {
+  console.log(result.reconstruction);
   parent.postMessage({ source: POST_MESSAGE_SOURCE, payload: result }, "*");
 });
 
