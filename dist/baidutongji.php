e102fe999ff97250e6850718a3c081f8a55f6bd1
cs0x7f <cs0x7f@gmail.com>
2024-05-01 23:39:55 +0800

    speedup page loading

--- dist/baidutongji.php
+++ dist/baidutongji.php
@@ -1,16 +1,21 @@
 <script type="text/javascript">
 var _hmt = _hmt || [];
-$(function() {
-	var hm = document.createElement("script");
-	hm.src = "//hm.baidu.com/hm.js?474c635761856d5056d56c73b5e2fc4b";
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(hm, s);
-	if (kernel.getProp && kernel.getProp('showad')) {
-		var ad = document.createElement("script");
-		ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6967007473251468';
-		ad.setAttribute('async', 'true');
-		ad.setAttribute('crossorigin', 'anonymous');
-		s.parentNode.insertBefore(ad, s);
+(function() {
+	var waitCnt = 0;
+	function asyncExec() {
+		if (++waitCnt < 5) {
+			$(asyncExec);
+			return;
+		}
+		setTimeout(function() {
+			DEBUG && console.log('[adstat] start loading async scripts');
+			var hm = document.createElement("script");
+			hm.src = "//hm.baidu.com/hm.js?474c635761856d5056d56c73b5e2fc4b";
+			hm.setAttribute('async', 'true');
+			var s = document.getElementsByTagName("script")[0];
+			s.parentNode.insertBefore(hm, s);
+		}, 500);
 	}
-});
+	$(asyncExec);
+})();
 </script>
